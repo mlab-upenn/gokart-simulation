78074ab152c787e92298516e1ad98c1b6b735065
Martin Endler <pokusew@seznam.cz>
2022-03-19 06:30:32 -0400

    simulator: Fix and improve gen_racetrack script (CSV loading fix, refactor code, SVG gen support)

--- simulator/scripts/gen_racetrack_stl.py
+++ simulator/scripts/gen_racetrack_stl.py
@@ -6,30 +6,37 @@ from scipy.interpolate import CubicSpline
 import numpy
 from stl import mesh
 import matplotlib.pyplot as plt
+from operator import itemgetter
 
 
 def load_wall(file):
-    with open(file, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        rows = list(spamreader)
-        wall = []
-        for row, idx in zip(rows, range(len(rows))):
-            if not idx:
-                long_idx = row.index('Longitude')
-                lat_idx = row.index('Latitude')
-            else:
-                wall.append([float(row[long_idx]), float(row[lat_idx]), 0.0])
-    return wall
+    points_with_ids = []
+    with open(file, newline='') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        for row in csv_reader:
+            id = row['Name']
+            long = row['Longitude']
+            lat = row['Latitude']
+            points_with_ids.append([int(id), [float(long), float(lat), 0.0]])
+    # https://stackoverflow.com/questions/17243620/operator-itemgetter-or-lambda
+    points_with_ids.sort(key=itemgetter(0))
+    points = numpy.empty((len(points_with_ids), 3), dtype=numpy.float64)
+    for (i, point) in enumerate(points_with_ids):
+        points[i] = point[1]
+    return points
 
 
 def trajectory_interpolate(points, int_size=20):
     _points = numpy.vstack((points, points[0, :]))
-
-    distance = numpy.cumsum(numpy.sqrt(numpy.sum(numpy.diff(_points, axis=0) ** 2, axis=1)))
+    distance = numpy.cumsum(numpy.sqrt(
+        numpy.sum(numpy.diff(_points, axis=0) ** 2, axis=1)
+    ))
     distance = numpy.insert(distance, 0, 0) / distance[-1]
     alpha = numpy.linspace(0, 1, int_size, endpoint=False)
-    ipol = CubicSpline(distance, _points, axis=0, bc_type="periodic")(alpha)
-    return ipol
+    points_interpolated = CubicSpline(
+        distance, _points, axis=0, bc_type='periodic'
+    )(alpha)
+    return points_interpolated
 
 
 def gnss_to_cartesian_test(point, offset, radius_north, radius_east):
@@ -43,16 +50,59 @@ def gnss_to_cartesian_test(point, offset, radius_north, radius_east):
 
 
 def gnss_to_cartesian(points, offset, radius_north, radius_east):
-    offset = [offset]
-    offset_ = numpy.repeat(offset, len(points), axis=0)
-    # remove offset
-    points_ = points - offset_
+    points_ = points - offset
     # degrees to radians
     points_ = numpy.radians(points_)
     # apply forward Equirectangular projection
     # https://en.wikipedia.org/wiki/Equirectangular_projection#Forward
-    points_ = points_ * numpy.repeat([[radius_east, radius_north, 1.0]], len(points), axis=0)
+    points_ = points_ * \
+        numpy.repeat([[radius_east, radius_north, 1.0]], len(points), axis=0)
     return points_
+
+
+def get_2d_in_3d_rotation_transform(angle):
+    '''Apply with numpy.dot(left_wall, R)'''
+    theta = numpy.radians(angle)
+    c, s = numpy.cos(theta), numpy.sin(theta)
+    R = numpy.array(((c, -s, 0.0), (s, c, 0.0), (0.0, 0.0, 1.0)))
+    return R
+
+
+def convert_points(
+    points_gps,
+    base_point_gps,
+    radius_north,
+    radius_east,
+    num_points=200,
+    visualize=True,
+):
+    points_xyz = gnss_to_cartesian(
+        points_gps,
+        base_point_gps,
+        radius_north,
+        radius_east,
+    )
+
+    if visualize:
+        plt.plot(
+            points_xyz[:, 0],  # x
+            points_xyz[:, 1],  # y
+        )
+        # add point numbers
+        for (i, p) in enumerate(points_xyz):
+            plt.text(p[0], p[1], str(i))
+
+    # no interpolation
+    if num_points is None:
+        return points_xyz
+
+    points_xyz_interpolated = trajectory_interpolate(points_xyz, num_points)
+    if visualize:
+        plt.plot(
+            points_xyz_interpolated[:, 0],  # x
+            points_xyz_interpolated[:, 1],  # y
+        )
+    return points_xyz_interpolated
 
 
 def create_stl(vertices, faces):
@@ -65,110 +115,117 @@ def create_stl(vertices, faces):
 
 def create_racetrack_faces(left_border, right_border):
     if len(left_border) != len(right_border):
-        print("Error")
-        sys.exit()
-
+        print('create_racetrack_faces: len(left_border) != len(right_border)')
+        sys.exit(1)
     faces = []
-
     triangle = [0, len(right_border), 1]
     faces.append(triangle)
     last_wall = 0
-
     for i in range(len(right_border) - 1):
         if last_wall:
             triangle = [triangle[0], triangle[2], triangle[0] + 1]
             faces.append(triangle)
             last_wall = 0
-
         if ~last_wall:
             triangle = [triangle[2], triangle[1], triangle[1] + 1]
             faces.append(triangle)
             last_wall = 1
-
     return faces
 
 
-if __name__ == "__main__":
-    left_wall = load_wall('purdue_left.csv')
-    right_wall = load_wall('purdue_right.csv')
-    # [longitude, latitude, elevation]
-    base_point_gps = [-86.945105, 40.437265, 0.0]  # GNSS  after transform coordinates of map frame origin [0,0,0]
-
-    # Constants from GPS Hector plugin
-    equatorial_radius = 6378137.0
-    flattening = 1.0 / 298.257223563
-    eccentricity2 = 2.0 * flattening - math.pow(flattening, 2.0)
-
-    # Calculate earth radius from GPS Hector plugin
-    base_point_latitude = math.radians(base_point_gps[1])
-    temp = 1.0 / (1.0 - eccentricity2 * math.pow(math.sin(base_point_latitude), 2.0))
-
-    prime_vertical_radius = equatorial_radius * math.sqrt(temp)
-    radius_north_ = prime_vertical_radius * (1.0 - eccentricity2) * temp
-    radius_east_ = prime_vertical_radius * math.cos(base_point_latitude)
-
-    # Pose.x: 55.043982949738 Pose.y: 45.211049994561
-    #         55.04398295343817,      45.21104993984664
-    # latitude: 40.437672148859 longitude: -86.944456253515
-
-    #out = gnss_to_cartesian_test(numpy.array([-86.944456253515, 40.437672148859, 0.0]), base_point_gps, radius_north_, radius_east_)
-    #out2 = gnss_to_cartesian(numpy.array([[-86.944456253515, 40.437672148859, 0.0]]), base_point_gps, radius_north_,
-    #                             radius_east_)
-
-    #print(f"OUT  {out}")
-    #print(f"OUT2 {out2[0]}")
-
-    #exit()
-
-    print(left_wall[0])
-    print(left_wall[20])
-    print(right_wall[0])
-    print(right_wall[20])
-
-    left_wall = gnss_to_cartesian(
-        numpy.array(left_wall),
-        base_point_gps,
-        radius_north_,
-        radius_east_
-    )
-
-    right_wall = gnss_to_cartesian(
-        numpy.array(right_wall),
-        base_point_gps,
-        radius_north_,
-        radius_east_
-    )
-
-    # exit()
-
-    theta = numpy.radians(0)
-    c, s = numpy.cos(theta), numpy.sin(theta)
-    R = numpy.array(((c, -s, 0.0), (s, c, 0.0), (0.0, 0.0, 1.0)))
-
-    plt.plot(left_wall[:, 0], left_wall[:, 1])
-    left_wall = trajectory_interpolate(left_wall, 200)
-    left_wall = numpy.vstack((left_wall, left_wall[0, :]))
-    left_wall = numpy.dot(left_wall, R)
-    plt.plot(left_wall[:, 0], left_wall[:, 1])
-
-    plt.plot(right_wall[:, 0], right_wall[:, 1])
-    right_wall = trajectory_interpolate(right_wall, 200)
-    right_wall = numpy.vstack((right_wall, right_wall[0, :]))
-    right_wall = numpy.dot(right_wall, R)
-    plt.plot(right_wall[:, 0], right_wall[:, 1])
-
-    print(left_wall[0])
-    print(left_wall[20])
-    print(right_wall[0])
-    print(right_wall[20])
-    plt.show()
-
-    vertices = [y for x in [left_wall, right_wall] for y in x]
+def create_stl_file(left_wall, right_wall, filename):
+    vertices = numpy.concatenate((left_wall, right_wall), axis=0)
     faces = create_racetrack_faces(left_wall, right_wall)
-
-    vertices = numpy.array(vertices)
     faces = numpy.array(faces)
 
     racetrack = create_stl(vertices=vertices, faces=faces)
-    # Write the mesh to file "cube.stl"
-    racetrack.save('racetrack.stl')
+    # write the mesh to a file
+    racetrack.save(filename)
+
+
+# TODO: replace hardcoded size
+def svg_with_path(path: str) -> str:
+    xml = f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="200000mm" height="200000mm" viewBox="0 0 200 200" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <path d="{path}" style="fill:rgb(255,82,31);"/>
+</svg>
+'''
+    return xml
+
+
+# TODO: replace hardcoded size
+def points_to_svg(left_points, right_points):
+    points_str_arr = []
+    for point in left_points:
+        x = point[0]
+        y = 200.0 - point[1]
+        points_str_arr.append(f'{x:.10f},{y:.10f}')
+    points_str = 'L'.join(points_str_arr)
+    left_path = f'M{points_str}Z'
+
+    points_str_arr = []
+    for point in right_points:
+        x = point[0]
+        y = 200.0 - point[1]
+        points_str_arr.append(f'{x:.10f},{y:.10f}')
+    points_str = 'L'.join(points_str_arr)
+    right_path = f'M{points_str}Z'
+
+    path = left_path + right_path
+
+    return svg_with_path(path)
+
+
+if __name__ == '__main__':
+    # INPUT:
+    left_wall_gps = load_wall('gps.local/purdue_left.csv')
+    right_wall_gps = load_wall('gps.local/purdue_right.csv')
+    # [longitude, latitude, elevation]
+    # GNSS  after transform coordinates of map frame origin [0,0,0]
+    base_point_gps_ = [-86.945105, 40.437265, 0.0]
+
+    # constants for from GPS Hector plugin
+    EQUATORIAL_RADIUS = 6378137.0
+    FLATTENING = 1.0 / 298.257223563
+    ECCENTRICITY2 = 2.0 * FLATTENING - math.pow(FLATTENING, 2.0)
+
+    # calculate earth radius from GPS Hector plugin
+    base_point_latitude_ = math.radians(base_point_gps_[1])
+    temp_ = 1.0 / (
+        1.0 - ECCENTRICITY2 * math.pow(math.sin(base_point_latitude_), 2.0)
+    )
+    prime_vertical_radius_ = EQUATORIAL_RADIUS * math.sqrt(temp_)
+    radius_north_ = prime_vertical_radius_ * (1.0 - ECCENTRICITY2) * temp_
+    radius_east_ = prime_vertical_radius_ * math.cos(base_point_latitude_)
+
+    left_wall_xyz = convert_points(
+        points_gps=left_wall_gps,
+        base_point_gps=base_point_gps_,
+        radius_north=radius_north_,
+        radius_east=radius_east_,
+        num_points=200,
+        visualize=True,
+    )
+
+    right_wall_xyz = convert_points(
+        points_gps=right_wall_gps,
+        base_point_gps=base_point_gps_,
+        radius_north=radius_north_,
+        radius_east=radius_east_,
+        num_points=200,
+        visualize=True,
+    )
+
+    # plt.show()
+
+    create_stl_file(
+        left_wall=left_wall_xyz,
+        right_wall=right_wall_xyz,
+        filename='gps.local/racetrack.stl',
+    )
+
+    svg = points_to_svg(left_wall_xyz, right_wall_xyz)
+    print(svg)
+
+pass
